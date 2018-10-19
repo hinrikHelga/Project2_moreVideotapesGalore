17e2ece2bd3bd2feaeb9701d08b867b99d030b08
Hinrik <hinrik.helga@gmail.com>
2018-10-19 13:27:00 +0000

    Re-initialize project.

--- MoreVideotapesGalore/MoreVideotapesGalore/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MoreVideotapesGalore.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MoreVideotapesGalore/MoreVideotapesGalore/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MoreVideotapesGalore.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
